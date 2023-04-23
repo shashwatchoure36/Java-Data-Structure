e7afc46f6aff234a456ead59a8412ce341195313
shashwatchoure365 <shashwatchoure36@gmail.com>
2023-04-23 19:50:48 +0530

    [Shashwat] Add : This Script is used to create LinkedList by add

--- LinkedList.java
+++ LinkedList.java
@@ -4,28 +4,29 @@ public class Linked_List {
 	Node next;
 	Node head;
 
-	// Node Class
+	//Node Class
 	class Node {
 		int data;
 		Node next;
 
-		// Parameterized Constructor
+		//Parameterized Constructor
 		Node(int x) {
 			data = x;
 			next = null;
 		}
 	}
-
+	
 	public Node insertNode(int data) {
 		Node new_node = new Node(data);
 		new_node.next = head;
 		head = new_node;
 		return head;
 	}
-
-	public void display() {
+	
+	public void display() 
+	{
 		Node node = head;
-		while (node != null) {
+		while(node!=null) {
 			System.out.print(node.data + " -> ");
 			node = node.next;
 		}
@@ -34,17 +35,14 @@ public class Linked_List {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		// TODO Auto-generated method stub
 		Linked_List l = new Linked_List();
 		System.out.println("Creating a Linked List");
-
-		l.insertNode(30);
-		l.insertNode(20);
+		
 		l.insertNode(50);
-
+		l.insertNode(40);
+		l.insertNode(47);
+		
 		l.display();
 
 	}
-
 }
